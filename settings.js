afed6e2b4d04d1ef1c1dca628c3e00a526afb890
bhanuraina <bhanuraina@gmail.com>
2021-02-13 09:18:50 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,17 +1,20 @@
 var settings = {
   development: {
     db: "notejam.db",
-    dsn: "sqlite://notejam.db"
+    dsn: "sqlite://notejam.db",
+  },
+  production: {
+    db: "notejam.db",
+    dsn: "sqlite://notejam.db",
   },
   test: {
     db: "notejam_test.db",
-    dsn: "sqlite://notejam_test.db"
-  }
+    dsn: "sqlite://notejam_test.db",
+  },
 };
 
-
-var env = process.env.NODE_ENV
+var env = process.env.NODE_ENV;
 if (!env) {
-  env = 'development'
-};
+  env = "production";
+}
 module.exports = settings[env];
